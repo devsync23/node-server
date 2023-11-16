b829af0a01711db699b013377ebc1225ab8796d8
Andrew Li <andrewliii199786@gmail.com>
2023-11-15 20:20:30 -0800

    issue with serialization

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('node:http');
 
 const server = http.createServer();
 
+
+
 const users = [
   { id: 1, name: "JB", age: 30 }
 ]
@@ -13,11 +15,33 @@ const routes = [
 // request handler
 function handleEndpoint(endpoint, request, response) {
   const { method } = request;
+  let body = [];
+  request.on('error', err =>{
+    console.error(err);
+  })
+  request.on('data', chunk =>{
+    body.push(chunk);
+  })
+  request.on('end', () =>{
+    body = Buffer.concat(body).toString();
+    console.log(body);
+  })
   // set up conditions for checking the route and the method
   if (endpoint === '/users') {
     if (method === 'GET') {
       response.end(JSON.stringify({ users }))
     }
+  }
+  if (method === "POST"){
+    const data = JSON.parse(body);
+    // for (const key in data){
+    //   if (data[key] === ''){
+    //     response.statusCode = 200;
+    //     response.end("data is missing fields/n")
+    //   }
+    // }
+    users.push(JSON.parse(body));
+    response.end(JSON.stringify({users}))
   }
 }
 
@@ -25,16 +49,14 @@ function handleEndpoint(endpoint, request, response) {
 server.on('request', (request, response) => {
 
   const endpoint = request.url;
-  if (routes.includes(endpoint)) {
-    // if the route is valid, pass it to the `handleEndpoint` function, along with the request and response objects
-    handleEndpoint(endpoint, request, response)
-  } else {
-    response.statusCode = 404;
-    response.end("Could not find route")
-  }
+    if (routes.includes(endpoint)) {
+      // if the route is valid, pass it to the `handleEndpoint` function, along with the request and response objects
+      handleEndpoint(endpoint, request, response)
+    } else {
+      response.statusCode = 404;
+      response.end("Could not find route")
+    }
 
-  // handle the response to the client
-  // response.end();
 })
 
 server.listen(3000, undefined, undefined, () => console.log("server is listening on port 3000..."))
