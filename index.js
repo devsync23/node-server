61ea5e86384d721b726251307c9dfdbc2292a57d
therealjustinbiele <justin.biele@gmail.com>
2023-11-16 17:46:38 -0800

    feat: add nodemode, handle post route

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 const http = require('node:http');
+const qs = require('node:querystring');
 
 const server = http.createServer();
 
 const users = [
   { id: 1, name: "JB", age: 30 }
 ]
+
 const routes = [
   '/users',
   '/signup'
 ]
 
-// request handler
-function handleEndpoint(endpoint, request, response) {
-  const { method } = request;
+
+// request handlerdsad
+function handleEndpoint(request, response) {
+  const { url, method } = request;
+
   // set up conditions for checking the route and the method
-  if (endpoint === '/users') {
+  if (url === '/users') {
     if (method === 'GET') {
-      response.end(JSON.stringify({ users }))
+      response.end(JSON.stringify(users))
+    } else if (method === 'POST') {
+      let body = [];
+      request
+        .on('data', chunk => body.push(chunk))
+        .on('end', () => {
+          body = JSON.parse(Buffer.concat(body).toString());
+          users.push(body)
+          response.end(JSON.stringify(users))
+        });
     }
   }
 }
 
 // .on( eventName: string, callback function )
 server.on('request', (request, response) => {
-
   const endpoint = request.url;
   if (routes.includes(endpoint)) {
     // if the route is valid, pass it to the `handleEndpoint` function, along with the request and response objects
-    handleEndpoint(endpoint, request, response)
+    handleEndpoint(request, response)
   } else {
     response.statusCode = 404;
     response.end("Could not find route")
